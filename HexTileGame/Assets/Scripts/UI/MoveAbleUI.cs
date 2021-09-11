bb3124b3178cb7217ba5abeb3553d4d11dbb06eb
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-09-11 21:37:36 +0900

    움직임 가능한 UI 구현 시도중 // 어제자인데 밀림 ㅋㅋ

--- HexTileGame/Assets/Scripts/UI/MoveAbleUI.cs
+++ HexTileGame/Assets/Scripts/UI/MoveAbleUI.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveAbleUI : MonoBehaviour
 {
+    [SerializeField] RectTransform topBar;
 
+    private void Start()
+    {
+        
+    }
 }
