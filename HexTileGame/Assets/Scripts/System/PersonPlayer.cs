647e9933b1420a429de9367443afb661e19823d3
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-09-26 21:25:33 +0900

    플레이어가 색깔을 갖도록 함

--- HexTileGame/Assets/Scripts/System/PersonPlayer.cs
+++ HexTileGame/Assets/Scripts/System/PersonPlayer.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PersonPlayer : PlayerScript
 {
     [SerializeField] string playerName;
+    [SerializeField] Color color;
 
     private void Start()
     {
         MainSceneManager.Instance.PlayerName = playerName;
+        base.playerColor = color;
         base.myName = playerName;
         base.Start();
     }
