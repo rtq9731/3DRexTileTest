d7e110892df66f70cbfdf9f3fb69c47518a3a3e9
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-09-26 21:24:10 +0900

    미사일 패널에 들어가 있는 UI용 스크립트 생성

---
+++ HexTileGame/Assets/Scripts/UI/PanelMissile.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelMissile : MonoBehaviour, ITurnFinishObj
+{
+    
+
+    public void TurnFinish()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ HexTileGame/Assets/Scripts/UI/PanelMissileQueue.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelMissileQueue : MonoBehaviour
+{
+    [SerializeField] Text textMissileName;
+    [SerializeField] Text textMissileMakeTime;
+    
+
+    public void SetData()
+    {
+
+    }
+}
