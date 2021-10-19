401296cb2d178666b402389d5493578952ec291a
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-10-19 22:31:26 +0900

    구름 프리팹 업데이트 및 구름 스크립트 정리

--- HexTileGame/Assets/Scripts/Object/CloudObject.cs
+++ HexTileGame/Assets/Scripts/Object/CloudObject.cs
@@ -5,6 +5,8 @@ using DG.Tweening;
 
 public class CloudObject : MonoBehaviour
 {
+    [SerializeField] float removeTime = 1f;
+
     [SerializeField] GameObject cloud1;
     [SerializeField] GameObject cloud2;
     [SerializeField] GameObject cloud3;
@@ -13,12 +15,14 @@ public class CloudObject : MonoBehaviour
     {
         Sequence sequence = DOTween.Sequence();
 
-        // 모든 구름들의 움직임 저장
-        sequence.Append(cloud1.transform.DOMove(new Vector3(cloud1.transform.position.x - 0.25f, cloud1.transform.position.y, cloud1.transform.position.z - 0.25f), 0.1f));
-        sequence.Join(cloud1.transform.DOScale(Vector3.zero, 0.1f));
-        sequence.Join(cloud2.transform.DOMove(new Vector3(cloud2.transform.position.x + 0.25f, cloud2.transform.position.y, cloud2.transform.position.z - 0.25f), 0.1f));
-        sequence.Join(cloud2.transform.DOScale(Vector3.zero, 0.1f));
-        sequence.Join(cloud3.transform.DOMove(new Vector3(cloud3.transform.position.x - 0.25f, cloud3.transform.position.y, cloud3.transform.position.z + 0.25f), 0.1f));
-        sequence.Join(cloud3.transform.DOScale(Vector3.zero, 0.1f));
+        // 모든 구름들을 각자의 방향으로 움직이고 줄여줌
+        sequence.Append(cloud1.transform.DOMove(new Vector3(cloud1.transform.position.x - 0.25f, cloud1.transform.position.y, cloud1.transform.position.z - 0.25f), removeTime)).SetEase(Ease.OutQuart);
+        sequence.Join(cloud1.transform.DOScale(Vector3.zero, removeTime));
+
+        sequence.Join(cloud2.transform.DOMove(new Vector3(cloud2.transform.position.x + 0.25f, cloud2.transform.position.y, cloud2.transform.position.z - 0.25f), removeTime)).SetEase(Ease.OutQuart);
+        sequence.Join(cloud2.transform.DOScale(Vector3.zero, removeTime));
+
+        sequence.Join(cloud3.transform.DOMove(new Vector3(cloud3.transform.position.x - 0.25f, cloud3.transform.position.y, cloud3.transform.position.z + 0.25f), removeTime)).SetEase(Ease.OutQuart);
+        sequence.Join(cloud3.transform.DOScale(Vector3.zero, removeTime));
     }
 }
