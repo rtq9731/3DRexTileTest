fddeb00d358d49e2223bc31543511ebd750d60cb
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-11-03 22:29:10 +0900

    타일이 가지던 공격력 값과 사거리값 삭제 및 적용

--- HexTileGame/Assets/01.Scripts/UI/TileInfoUI/SimpleTileInfoPanel.cs
+++ HexTileGame/Assets/01.Scripts/UI/TileInfoUI/SimpleTileInfoPanel.cs
@@ -9,9 +9,7 @@ public class SimpleTileInfoPanel : MonoBehaviour
     [SerializeField] Text ownerText;
     [SerializeField] Text groundTypeText;
     [SerializeField] Text priceText;
-    [SerializeField] Text rangeText;
     [SerializeField] Text shieldText;
-    [SerializeField] Text attackPowerText;
     [SerializeField] Text productText;
     [SerializeField] float alphaOnTime = 0.5f;
 
@@ -55,9 +53,7 @@ public class SimpleTileInfoPanel : MonoBehaviour
         ownerText.text = $"소유자 : {ownerName}";
         groundTypeText.text = $"지형 : {data.type}";
         priceText.text = $"가격 : {data.Price}";
-        rangeText.text = $"사거리 : {data.Range}";
         shieldText.text = $"방어력 : {data.Shield}";
-        attackPowerText.text = $"공격력 : {data.AttackPower}";
         productText.text = $"생산 : {data.Resource}";
     }
 
--- HexTileGame/Assets/01.Scripts/VO/TileData.cs
+++ HexTileGame/Assets/01.Scripts/VO/TileData.cs
@@ -11,20 +11,16 @@ public class TileData
     Vector3 position = Vector3.zero;
 
     int maxShield = 100;
-    int range = 3;
     int price = 20;
     int shield = 100;
     int maxResource = 3;
     int resource = 3;
-    int attackPower = 0;
 
     public int MaxShield { get { return maxShield; } set { maxShield = value; } }
-    public int Range { get { return range; } set { range = value; } }
     public int Price { get { return price; } set { price = value; } }
     public int Shield { get { return shield; } set { shield = value; } }
     public int MaxResource { get { return maxResource; } set { maxResource = value; } }
     public int Resource { get { return resource; } set { resource = value; } }
-    public int AttackPower { get { return attackPower; } set { attackPower = value; } }
 
     public Vector3 Position { get { return position; } set { position = value; } }
 
@@ -34,14 +30,12 @@ public class TileData
         type = TileType.Forest;
         maxShield += 10;
         shield = maxShield;
-        range -= 1;
     }
 
     public void SetDataToMountain()
     {
         SetDataToPlain();
         type = TileType.Mountain;
-        range += 1;
         maxResource -= 1;
         resource -= 1;
     }
@@ -52,19 +46,16 @@ public class TileData
         type = TileType.DigSite;
         maxResource += 1;
         resource += 1;
-        range -= 1;
     }
 
     public void SetDataToRock()
     {
         maxShield += 30;
         shield = maxShield;
-        attackPower -= 1;
     }
 
     private void SetDataToPlain() // 지형 초기화용 함수
     {
-        range = 3;
         resource = 3;
         shield = 100;
     }
