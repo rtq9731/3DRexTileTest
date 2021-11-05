d3e82067716fc37b8de16fc8e3cc2131a81f2193
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-11-05 09:56:20 +0900

    UI 변동사항 원래 씬에 적용

--- HexTileGame/Assets/01.Scripts/System/PersonPlayer.cs
+++ HexTileGame/Assets/01.Scripts/System/PersonPlayer.cs
@@ -85,7 +85,6 @@ public class PersonPlayer : PlayerScript
     {
         get 
         {
-            Debug.Log(playerData.CurResearchData);
             return playerData.CurResearchData; 
         }
         set
