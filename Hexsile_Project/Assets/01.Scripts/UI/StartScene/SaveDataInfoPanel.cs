f1d8886a837b78073f5babefca0e71badc303cc8
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-11-15 18:43:31 +0900

    세이브 삭제 기능 추가

--- Hexsile_Project/Assets/01.Scripts/UI/StartScene/SaveDataInfoPanel.cs
+++ Hexsile_Project/Assets/01.Scripts/UI/StartScene/SaveDataInfoPanel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,7 @@ public class SaveDataInfoPanel : MonoBehaviour
 {
 
     [SerializeField] Button btnLoad = null;
+    [SerializeField] Button btnDelete = null;
     [SerializeField] Text textPlayerName = null;
     [SerializeField] Text textPlayerResource = null;
     [SerializeField] Text textFireableMissiles = null;
@@ -16,12 +18,19 @@ public class SaveDataInfoPanel : MonoBehaviour
 
     void Start()
     {
+        UpdateSaveDataInfoToNull();
+    }
+
+    public void UpdateSaveDataInfoToNull()
+    {
         textPlayerName.text = "세이브파일 선택되지 않음!";
         textPlayerResource.text = $"현재 가진 자원 : ";
         textFireableMissiles.text = $"발사 가능한 미사일 수 : ";
         textSaveDateTime.text = $"저장된 시각 : ";
         textStage.text = $"스테이지 : ";
         playerColorImage.color = Color.white;
+
+        btnDelete.gameObject.SetActive(false);
     }
 
     public void UpdateSaveDataInfo(SaveData data)
@@ -33,6 +42,16 @@ public class SaveDataInfoPanel : MonoBehaviour
         textStage.text = $"스테이지 : {data.stageCount}";
         playerColorImage.color = data.playerData.PlayerColor;
 
-        btnLoad.onClick.AddListener(() => GameManager.Instance.GameStartWithLoad(data));
+        btnLoad.onClick.RemoveAllListeners();
+        btnLoad.onClick.AddListener(() => GameManager.Instance.StartLoadedGame(data));
+
+        btnDelete.gameObject.SetActive(true);
+        btnDelete.onClick.RemoveAllListeners();
+        btnDelete.onClick.AddListener(() =>
+        {
+            FindObjectsOfType<SaveInfoPanel>().ToList().FindAll(x => x.data == data).ForEach(x => Destroy(x.gameObject));
+            UpdateSaveDataInfoToNull();
+            GameManager.Instance.DeleteFile(data);
+        });
     }
 }
